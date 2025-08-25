6c66aaf8b2e5d75cf2f242fee4c3943dbacf2bd3
Landon94 <lkarkut@uci.edu>
2025-08-25 12:28:34 -0700

    Implemented video loop

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+def main():
+
+    while True:
+
+        ret,frame = cap.read()
+
+        cv2.imshow('frame',frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    main()
