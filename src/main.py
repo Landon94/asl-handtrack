178d301af713236de409a8183fce0332c6880176
Landon94 <lkarkut@uci.edu>
2025-08-27 10:35:18 -0700

    Implemented media pipe with hand recognition.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,36 @@
 import cv2
+import time
+import mediapipe as mp
+import numpy as np
 
-cap = cv2.VideoCapture(0)
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture(1)
 
 def main():
+
+    hand = mp_hands.Hands()
 
     while True:
 
         ret,frame = cap.read()
 
-        cv2.imshow('frame',frame)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        result = hand.process(rgb_frame)
+
+        if result.multi_hand_landmarks:
+            for hand_landmarks in result.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(rgb_frame,hand_landmarks,
+                                        mp_hands.HAND_CONNECTIONS,
+                                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                                        mp_drawing_styles.get_default_hand_connections_style()
+                                        )
+
+        final_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
+        cv2.imshow("image",cv2.flip(final_frame,1))
 
         if cv2.waitKey(1) & 0xFF == 27:
             break
