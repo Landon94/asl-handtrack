abb126c04ba53e9288ecd3da6246ee3c36892438
Landon94 <lkarkut@uci.edu>
2025-08-28 20:11:39 -0700

    Model now used to predict video.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,15 @@ import cv2
 import time
 import mediapipe as mp
 import numpy as np
+import tensorflow as tf
+from model import create_model
+
+CLASS_NAMES = [
+    "A","B","Blank","C","D","E","F","G","H","I","J",
+    "K","L","M","N","O","P","Q","R","S","T",
+    "U","V","W","X","Y","Z",
+]
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -12,6 +21,9 @@ cap = cv2.VideoCapture(1)
 def main():
 
     hand = mp_hands.Hands()
+
+    model = create_model((180,180,3))
+    model.load_weights("asl_cnn.h5")
 
     while True:
 
@@ -36,7 +48,26 @@ def main():
                 x_min, x_max = int(min(xs)), int(max(xs))
                 y_min, y_max = int(min(ys)), int(max(ys))
 
+                cropped_roi = frame[y_min-100:y_max+100, x_min-100:x_max+100]
+
+                roi_rgb = cv2.cvtColor(cropped_roi, cv2.COLOR_BGR2RGB)
+
+                resized_frame = cv2.resize(roi_rgb,(180,180),interpolation=cv2.INTER_AREA)
+                resized_frame = np.expand_dims(resized_frame, axis=0)
+
+                cv2.imshow("ROI", cropped_roi)
+    
+                preds = model.predict(resized_frame)
+                cls_idx = int(np.argmax(preds[0]))
+                conf = float(np.max(preds[0]))
+                # print(predictions)
+
+                label = CLASS_NAMES[cls_idx]
+                # print(label)
+                # print(f"{cls_idx=} {conf=:.3f}")
+
                 cv2.rectangle(rgb_frame,pt1=(x_min-30,y_min-30),pt2=(x_max+30,y_max+30),color=(0,255,0),thickness=2)
+                cv2.putText(rgb_frame,label,(x_min-50,y_min-50),cv2.FONT_HERSHEY_SIMPLEX,2.0,(255,0,0),3,cv2.LINE_AA)
 
         final_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
         cv2.imshow("image",cv2.flip(final_frame,1))
